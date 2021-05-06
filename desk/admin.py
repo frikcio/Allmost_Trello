cb05e3f6f6ad84a4eb51d19318b025863beb1221
frikcio <frikcio67@gmail.com>
2021-05-06 21:01:00 +0300

    added user's list

--- desk/admin.py
+++ desk/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import TaskModel
+from .models import TaskModel, TrelloUser
+
 # Register your models here.
 
 admin.site.register(TaskModel)
+admin.site.register(TrelloUser)
