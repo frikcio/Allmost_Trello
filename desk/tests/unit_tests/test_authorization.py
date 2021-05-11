c23f19b85e9bed3c40d3141c603cc3e51f3dc081
frikcio <frikcio67@gmail.com>
2021-05-11 15:56:29 +0300

    added tests for authentication, creation card and changing card's status

--- desk/tests/unit_tests/test_authorization.py
+++ desk/tests/unit_tests/test_authorization.py
@@ -1,25 +1,13 @@
 from django.test import TestCase
 
 from desk.models import TrelloUser
-from desk.views import Login
 
 
 class TestLogin(TestCase):
     def setUp(self):
-        self.user = TrelloUser.objects.create_user(username="foxana", password="52654936abcdA")
+        self.user = TrelloUser.objects.create_user(username="frikcio", password="0000")
+        self.response = self.client.post('/login/', data={'username': 'frikcio',
+                                                          'password': '0000'})
 
     def test_success_url(self):
-        request = self.client.post('/login/', data={'username': 'foxana',
-                                                     'password': '52654936abcdA'})
-        request.user = self.user
-        view = Login()
-        view.setup(request)
-        self.assertEqual(f'/profile/{request.user.id}/', view.get_success_url())
-
-    def test_fail_success_url(self):
-        request = self.client.post('/login/', data={'username': 'foxana',
-                                                    'password': '52654936abcdA'})
-        request.user = self.user
-        view = Login()
-        view.setup(request)
-        self.assertNotEqual(f'/login/', view.get_success_url())
+        self.assertRedirects(response=self.response, expected_url=f'/profile/{self.user.pk}/')
--- desk/tests/unit_tests/test_cards.py
+++ desk/tests/unit_tests/test_cards.py
@@ -5,43 +5,72 @@ from desk.views import *
 
 class TestCreateCard(TestCase):
     def setUp(self):
+        self.user1 = TrelloUser.objects.create_user(username="frikcio", password="0000")
+        self.user2 = TrelloUser.objects.create_user(username="foxana", password="0000")
+        self.data = {'title': 'test_card', 'text': 'some-text', 'performer': f"{self.user1.pk}"}
+        self.request = RequestFactory().post('/card/create/', data=self.data)
+        self.request.user = self.user1
         self.view = CreateCardView()
-        self.request = RequestFactory().post('/card/create/', data={'title': 'test_card',
-                                                                    'text': 'some-text',
-                                                                    'performer': ''})
-        self.request.user = TrelloUser.objects.create_user(username="foxana", password="52654936abcdA")
 
     def test_success_url(self):
-        self.view.setup(self.request)
-        self.assertEqual('/', self.view.get_success_url())
+        self.client.force_login(self.user1)
+        response = self.client.post('/card/create/', data=self.data)
+        self.assertRedirects(response=response, expected_url='/', status_code=302)
 
     def test_owner_in_form_kwargs(self):
         self.view.setup(self.request)
         kwargs = self.view.get_form_kwargs()
         self.assertIn('owner', kwargs)
 
-    def test_form_valid(self):
+    def test_owner_is_user_(self):
         self.view.setup(self.request)
-        form = self.view.get_form()
-        self.assertEquals(form.owner, self.request.user)
+        self.view.form_valid(self.view.get_form())
+        card = TaskModel.objects.get(title=self.data['title'])
+        self.assertEquals(card.owner, self.request.user)
+
+    def test_performer_is_owner(self):
+        self.view.setup(self.request)
+        self.view.form_valid(self.view.get_form())
+        card = TaskModel.objects.get(title=self.data['title'])
+        self.assertEquals(card.performer, self.request.user)
 
 
-class TestRaiseStatus(TestCase):
+class TestChangeStatus(TestCase):
     def setUp(self):
         self.user1 = TrelloUser.objects.create_user(username="foxana", password="0000")
         self.user2 = TrelloUser.objects.create_user(username="frikcio", password="0000")
         self.admin = TrelloUser.objects.create_superuser(username='admin', password='admin')
-        self.card = TaskModel.objects.create(title="test", text="bla pla", performer=self.user2, owner=self.user1)
-        self.request = RequestFactory().post(f"/card/status/raise/{self.card.pk}/")
+        self.card = TaskModel.objects.create(title="test", text="bla pla", performer=self.user2, owner=self.user1,
+                                             status=3)
+        self.request1 = RequestFactory().post(f"/card/status/raise/{self.card.pk}/")
+        self.request1.user = self.user2
+        self.request2 = RequestFactory().post(f"/card/status/omit/{self.card.pk}/")
         self.view = RaiseStatusView()
 
-    def test_success_url(self):
-        self.view.setup(self.request)
-        self.assertEqual('/', self.view.get_success_url())
+    def test_success_url(self):     # Test if all are good, all are work)
+        self.client.force_login(self.user2)
+        response = self.client.post(f"/card/status/raise/{self.card.pk}/")
+        self.assertRedirects(response=response, expected_url='/', status_code=302)
+        response = self.client.post(f"/card/status/omit/{self.card.pk}/")
+        self.assertRedirects(response=response, expected_url='/', status_code=302)
 
-    def test_form_valid(self):
-        self.view.setup(self.request, pk=self.card.pk)
-        starting_status = self.view.get_object().status
-        self.view.post(self.request)
-        finish_status = self.view.get_object().status
-        self.assertNotEquals(starting_status, finish_status)
+    def test_user_is_performer(self):   # Test if request.user is performer or admin, status will changed
+        self.client.force_login(self.user2)
+        begin_status = TaskModel.objects.get(pk=self.card.pk).status
+        self.client.post(f"/card/status/raise/{self.card.pk}/")     # status increase by 1
+        finish_status = TaskModel.objects.get(pk=self.card.pk).status
+        self.assertNotEquals(begin_status, finish_status)
+        self.client.post(f"/card/status/omit/{self.card.pk}/")      # status decrease by 1
+        another_finish_status = TaskModel.objects.get(pk=self.card.pk).status
+        self.assertEquals(begin_status, another_finish_status)
+
+    def test_user_is_not_performer(self):   # Test if request.user is not performer or admin, status will not changed
+        self.client.force_login(self.user1)
+        begin_status = self.card.status
+        self.client.post(f"/card/status/raise/{self.card.pk}/")
+        finish_status = self.card.status
+        self.assertEqual(begin_status, finish_status)
+        self.client.post(f"/card/status/omit/{self.card.pk}/")
+        finish_status = self.card.status
+        self.assertEqual(begin_status, finish_status)
+
